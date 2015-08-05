f2d4ef6f0891af4d25e304813e92ce5c9819f83a
heishou <18608566147@163.com>
2015-08-05 11:13:53 +0800

    首次提交

--- 反射/src/com/heishou/Ihello.java
+++
@@ -1,8 +0,0 @@
-package com.heishou;
-
-public interface Ihello {
-   public static final String author="黑手";
-   public static int age=100;
-   public void sayhello();
-   public void saygepi(int age);
-}
